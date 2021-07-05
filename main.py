2367a33704fa90ffe399c540a3dd5f8a942ad4c1
Christian Gennari <christiangennari61@gmail.com>
2021-07-06 00:24:37 +0200

    Improved GUI and disabled textview editing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-from tkinter.constants import BOTTOM, END, INSERT, LEFT, RIGHT, TOP
+from tkinter.constants import BOTH, BOTTOM, DISABLED, END, INSERT, LEFT, NORMAL, RIDGE, RIGHT, TOP, X, Y
 import PasswordChecker
 import tkinter
 from tkinter import filedialog
@@ -19,48 +19,61 @@ def main():
 
 
     # TEXTVIEW UI
-    textView = tkinter.Text(GUI_Window)
+    textView = tkinter.Text(GUI_Window, bg="DarkSlategray")
+    textView.config(state=DISABLED)
     textView.pack()
 
-
-    # PASSWORD AMOUNT ENTRY UI
-    amountOfPasswordsLabel = tkinter.Label(text="AMOUNT OF PASSWORDS: ")
-    amountOfPasswordsLabel.pack(ipadx=10, ipady=10, side=LEFT)
-    amountOfPasswords = tkinter.StringVar()
-    amountOfPasswordsEntry = tkinter.Entry(GUI_Window, textvariable=amountOfPasswords)
-    amountOfPasswordsEntry.pack(ipadx=10, ipady=4, side=LEFT)
-
-
-    # PASSWORD LENGTH ENTRY UI
-    passwordLengthLabel = tkinter.Label(text="LENGTH OF PASSWORDS: ")
-    passwordLengthLabel.pack(ipadx=10, ipady=10, side=LEFT)
-    passwordLength = tkinter.StringVar()
-    passwordLengthEntry = tkinter.Entry(GUI_Window, textvariable=passwordLength)
-    passwordLengthEntry.pack(ipadx=10, ipady=4, side=LEFT)
-
-
-    # SAVES TXT FILE
-    def savePasswords():
-        saveFile = filedialog.asksaveasfile(mode='w', defaultextension='.txt')
-        if saveFile == None:
-            return
-        saveFile.write(textView.get(1.0, END))
-        saveFile.close()
-    saveButton = tkinter.Button(GUI_Window, text="Save! (.txt)", command=savePasswords)
-    saveButton.pack(ipadx=198, ipady=10, side=BOTTOM)
 
     def generateButton():
         generatedPasswords = generatePassword()
         PasswordChecker.checkPassword(generatedPasswords)
 
         # Clears previous text and shows passwords in textView
+        textView.config(state=NORMAL)
+
         textView.delete(1.0, END)
         pwdID = 0
         for each in generatedPasswords:
             pwdID += 1
             textView.insert(INSERT,f"{str(pwdID)}: {str(each)}\n")
-    generateButton = tkinter.Button(GUI_Window, text="Generate!", command=generateButton)
-    generateButton.pack(ipadx=200, ipady=10, side=BOTTOM)  
+        
+        textView.config(state=DISABLED)
+    generateButton = tkinter.Button(GUI_Window, text="Generate!", command=generateButton, bg="darkred")
+    generateButton.pack(side=RIGHT, ipadx=100, ipady=30, padx=20) 
+    
+    # SAVES TXT FILE
+    def savePasswords():
+
+        if textView.compare("end-1c", "==", "1.0"):
+            return
+        else:
+            textView.config(state=NORMAL)
+
+            saveFile = filedialog.asksaveasfile(mode='w', defaultextension='.txt')
+            if saveFile == None:
+                return
+            saveFile.write(textView.get(1.0, END))
+            saveFile.close()
+
+        textView.config(state=DISABLED)
+    saveButton = tkinter.Button(GUI_Window, text="Save! (.txt)", command=savePasswords, bg="DarkSlateGrey")
+    saveButton.pack(side=RIGHT, ipady=30, ipadx=30, padx=15)
+
+
+     # PASSWORD AMOUNT ENTRY UI
+    amountOfPasswordsLabel = tkinter.Label(text="AMOUNT OF PASSWORDS: ")
+    amountOfPasswordsLabel.pack(side=LEFT, padx=10)
+    amountOfPasswords = tkinter.StringVar()
+    amountOfPasswordsEntry = tkinter.Entry(GUI_Window, textvariable=amountOfPasswords, bg="DarkSlategray")
+    amountOfPasswordsEntry.pack(side=LEFT)
+
+    # PASSWORD LENGTH ENTRY UI
+    passwordLengthLabel = tkinter.Label(text="LENGTH: ")
+    passwordLengthLabel.pack(side=LEFT)
+    passwordLength = tkinter.StringVar()
+    passwordLengthEntry = tkinter.Entry(GUI_Window, textvariable=passwordLength, bg="DarkSlategray")
+    passwordLengthEntry.pack(side=LEFT, padx=10)
+
         
     # PASSWORD GENERATION
     def generatePassword():
